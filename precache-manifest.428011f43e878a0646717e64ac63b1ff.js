8f392830ca909e36f528a271cc92e17b075c63fe
decentralMind <decentralmind@gmail.com>
2019-04-24 21:02:42 +0545

    Updates

---
+++ precache-manifest.428011f43e878a0646717e64ac63b1ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5907cbd514491d8e9cf",
+    "url": "/kenjirai/signthedoc/static/js/runtime~main.5f655995.js"
+  },
+  {
+    "revision": "d15fb5dfbfa590dcf679",
+    "url": "/kenjirai/signthedoc/static/js/main.8c3a4c5c.chunk.js"
+  },
+  {
+    "revision": "717efa130808a36b8fee",
+    "url": "/kenjirai/signthedoc/static/js/2.9c49b5d1.chunk.js"
+  },
+  {
+    "revision": "d15fb5dfbfa590dcf679",
+    "url": "/kenjirai/signthedoc/static/css/main.06462424.chunk.css"
+  },
+  {
+    "revision": "717efa130808a36b8fee",
+    "url": "/kenjirai/signthedoc/static/css/2.f56ec393.chunk.css"
+  },
+  {
+    "revision": "ed655737d8535711b9cf00ef75a25dd0",
+    "url": "/kenjirai/signthedoc/index.html"
+  }
+];
